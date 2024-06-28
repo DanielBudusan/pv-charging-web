f7d04efd8918fabc3b374f091f634f0fb152bc32
Daniel Budusan <danibudusan@gmail.com>
2024-06-28 17:24:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>PV Charging is an iOS app to sync your PV system with your EV charging</p>
+        <p>App under construction, stay tuned</p>
       </header>
+      <body>
+      <h3> &copy; 2024 PV Charging</h3>
+      </body>
     </div>
   );
 }
